c7729c088be200311adfd6e32435693a730f7517
Ahmed Mohamed <a7m3d.219@gmail.com>
2022-03-25 19:53:10 +0200

    edit fetch data

--- controllers/replys.js
+++ controllers/replys.js
@@ -15,15 +15,7 @@ const ayah = async (ctx) => {
     <strong>${ayahText}</strong>
 
     [${surahName}] - أيه رقم: ${ayahNumber} - عدد أياتها: ${ayahsNumber} - الجزء: ${juzNumber} - رقم الصفحة: ${pageNumber}
-    
-  
     `);
-    // await ctx.replyWithHTML(`<strong>${ayahText}</strong>`);
-    // await ctx.reply(surahName);
-    // await ctx.reply(`أية رقم: ${ayahNumber}`);
-    // await ctx.reply(`عدد أياتها: ${ayahsNumber}`);
-    // await ctx.reply(`الجزء: ${juzNumber}`);
-    // await ctx.reply(`رقم الصفحة: ${pageNumber}`);
   } catch (error) {
     ctx.reply("هناك خطأ ما!");
   }
