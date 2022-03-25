cb2032aa6cbcd95f4fc416fe83f69c78f48b8a7a
Ahmed Mohamed <a7m3d.219@gmail.com>
2022-03-25 16:47:11 +0200

    add wenHook to app

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
+const { Telegraf } = require("telegraf");
 
 const commandsController = require("./controllers/commands");
 const replysController = require("./controllers/replys");
 const errorsController = require("./controllers/errors");
 
-const { Telegraf } = require("telegraf");
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
 const port = process.env.PORT || 3000;
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.telegram.setWebhook(
-  "https://ayah-bot.herokuapp.com/" + process.env.BOT_TOKEN
-);
+// bot.telegram.setWebhook(
+//   "https://ayah-bot.herokuapp.com/" + process.env.BOT_TOKEN
+// );
 
 // if (process.env.NODE_ENV === "production") {
 //   bot = new TelegramBot(process.env.BOT_TOKEN).listen(port);
@@ -42,6 +37,12 @@ bot.on("message", errorsController.message);
 // process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 const app = express();
-app.use(bot.webhookCallback(process.env.BOT_TOKEN));
+// app.use(bot.webhookCallback(process.env.BOT_TOKEN));
+bot.launch({
+  webhook: {
+    domain: "https://ayah-bot.herokuapp.com/",
+  },
+});
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port);
