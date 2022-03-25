a1675ac5b69cb4c06d88ae4f8e1ecc216484164e
Ahmed Mohamed <a7m3d.219@gmail.com>
2022-03-25 16:24:26 +0200

    add wenHook to app

--- server.js
+++ server.js
@@ -13,13 +13,16 @@ app.use(bodyParser.json());
 
 let bot;
 const port = process.env.PORT || 3000;
+bot = new Telegraf(process.env.BOT_TOKEN);
+// if (process.env.NODE_ENV === "production") {
+//   bot = new TelegramBot(process.env.BOT_TOKEN).listen(port);
+//   bot.setWebHook(process.env.HEROKU_URL + process.env.BOT_TOKEN);
+// } else {
+//   bot = new Telegraf(process.env.BOT_TOKEN);
+// }
 
-if (process.env.NODE_ENV === "production") {
-  bot = new TelegramBot(process.env.BOT_TOKEN);
-  bot.setWebHook(process.env.HEROKU_URL+process.env.BOT_TOKEN);
-} else {
-  bot = new Telegraf(process.env.BOT_TOKEN);
-}
+app.use(bot.webhookCallback(process.env.BOT_TOKEN));
+bot.telegram.setWebhook(process.env.HEROKU_URL + process.env.BOT_TOKEN);
 
 // Commands
 bot.start(commandsController.start);
@@ -37,3 +40,5 @@ bot.launch();
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+app.listen(port);
