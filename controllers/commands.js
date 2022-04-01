b33011e912c9ba742baeaf3210610e0b311d01c4
Ahmed Mohamed <a7m3d.219@gmail.com>
2022-04-01 17:28:18 +0200

    edit start command

--- controllers/commands.js
+++ controllers/commands.js
@@ -1,6 +1,10 @@
 const start = async (ctx) => {
-  await ctx.reply("Ａｙａｈ Ｂｏｔ أهلا بك في");
-  await ctx.reply("لتحصل علي أيه اضغط علي الزر الظاهر أمامك)");
+  await ctx.reply("(Ayah Bot) أهلا بك في");
+  await ctx.telegram.sendMessage(
+    ctx.chat.id,
+    "لتحصل علي أيه اضغط علي الزر الظاهر أمامك أو اكتب كلمة (آية))",
+    keyboardBtns
+  );
 };
 
 module.exports = {
